784fd8c78428e58ab4ffc243879eb70aa27d86dc
GongBJ <gongbj0113@gmail.com>
2023-12-18 01:58:29 +0900

    first

---
+++ python/generator.py
@@ -0,0 +1,43 @@
+from PIL import Image, ImageDraw, ImageFont
+import imageio.v2 as imageio
+import json
+import uuid
+
+def create_gif_with_korean_text(text, json_path, frames_path, output_path):
+    # Read the JSON file
+    with open(json_path, 'r') as file:
+        json_data = json.load(file)
+
+    frames = []
+
+    for frame_data in json_data:
+        # Load the image
+        image_path = f"{frames_path}/{frame_data['imageName']}"
+        image = Image.open(image_path)
+
+        # Draw the text
+        draw = ImageDraw.Draw(image)
+        font_size = frame_data['bounding_box']['height']
+        font = ImageFont.truetype('./resources/NotoSansKR-Bold.ttf', int(font_size / 2))
+
+        bbox = frame_data['bounding_box']
+        draw.text((bbox['left'], bbox['top']), text, fill="black", font=font)
+
+        # Save the frame
+        temp_path = f"{frames_path}/temp_{frame_data['imageName']}"
+        image.save(temp_path)
+        frames.append(imageio.imread(temp_path))
+
+    # Create GIF
+    filename = output_path + uuid.uuid4().hex + '.gif'
+    
+    imageio.mimsave(filename, frames, duration=0.06)  # Duration in seconds per frame
+    
+    # sysout the filename
+    print(filename)
+
+if __name__ == '__main__':
+    # take text argument
+    import sys
+    text = sys.argv[1]
+    create_gif_with_korean_text(text, './resources/data.json', './resources/frames', './output/')
---
+++ src/app/api/generate/route.js
@@ -0,0 +1,54 @@
+import { exec } from "child_process";
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+import { NextResponse } from "next/server";
+
+const execAsync = promisify(exec);
+const readFileAsync = promisify(fs.readFile);
+const unlinkAsync = promisify(fs.unlink);
+
+export async function POST(req) {
+  // application/x-www-form-urlencoded
+  // get the 'text' from the request data
+  let text = (await req.json())["text"];
+  if (!text) {
+    text = "치타는 달린다";
+  }
+
+  try {
+    // Execute Python script
+    const { stdout } = await execAsync(`python python/generator.py "${text}"`);
+    const filePath = path.join(process.cwd(), stdout.trim());
+
+    // Read the output file as a Buffer
+    const buffer = await readFileAsync(filePath);
+
+    // Delete the file after reading
+    await unlinkAsync(filePath);
+
+    // Return the successful response with binary data
+    return new NextResponse(buffer, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/octet-stream",
+        "Content-Disposition": `attachment; filename="${path.basename(
+          filePath
+        )}"`,
+      },
+    });
+  } catch (error) {
+    console.error(`Error: ${error}`);
+
+    // Return error response
+    return new NextResponse(
+      JSON.stringify({ message: "Internal Server Error" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
